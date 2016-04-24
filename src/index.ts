ac9ef7a76eaed5d4763c8010a10d5f94cc3666bc
jelisejev <pavel.yeliseev@ctco.lv>
2016-04-24 21:35:37 +0300

    initial backend integration

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 class CssStatsComponent {
+  private api: string = 'http://localhost:8080';
+
   init(container: HTMLElement) {
-    container.getElementsByTagName('form')[0].addEventListener('submit', () => {
-      alert('z');
+    $('form').submit(() => {
+      this.loadStats($('input').val());
+
+      return false;
     });
+  }
+
+  loadStats(url: string) {
+    return this.getStats(url).then((data) => {
+      $('.data').html(Mustache.render('<pre>{{data}}</pre>', {
+        data: JSON.stringify(data)
+      }));
+    });
+  }
+
+  private getStats(url) {
+    return $.get(this.api + '/stats', { url });
   }
 }
 
