07927d7fcaad1f94602184506bc4dabf662427cf
jelisejev <pavel.yeliseev@ctco.lv>
2016-04-25 21:57:09 +0300

    removed templates

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,31 @@
 class CssStatsComponent {
   private api: string = 'http://localhost:8080';
+  
+  public constructor(private elem: JQuery) {
+  }
 
-  init(container: HTMLElement) {
-    $('form').submit(() => {
-      this.loadStats($('input').val());
-
-      return false;
-    });
+  render() {
+    this.renderForm();
+  }
+  
+  private renderForm() {
+    const inputGroup = $('<div class="input-group"></div>')
+      .append('<input type="text" class="form-control" value="http://twitter.com">')
+      .appendTo(this.elem);
+    
+    $('<span class="input-group-btn"></span>')
+        .append('<button class="btn btn-default" type="button">Analyze</button>')
+        .click(() => this.loadStats($('input', this.elem).val()))
+        .appendTo(inputGroup);
+  }
+  
+  private renderResults(data) {
+    $('<pre></pre>').text(JSON.stringify(data)).appendTo(this.elem);  
   }
 
   loadStats(url: string) {
-    return this.getStats(url).then((data) => {
-      $('.data').html(Mustache.render('<pre>{{data}}</pre>', {
-        data: JSON.stringify(data)
-      }));
-    });
-  }
-
-  private getStats(url) {
-    return $.get(this.api + '/stats', { url });
+    return $.get(this.api + '/stats', { url }).then((data) => this.renderResults(data));
   }
 }
 
-new CssStatsComponent().init(document.getElementById('css-stats'));
+new CssStatsComponent($('#css-stats')).render();
