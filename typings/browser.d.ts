0c5981b2ca744e0ebf1e5786004ca60c92588a9f
jelisejev <pavel.yeliseev@ctco.lv>
2016-04-26 21:36:03 +0300

    removed unused typings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,4 +1,2 @@
-/// <reference path="browser/ambient/chartjs/index.d.ts" />
 /// <reference path="browser/ambient/highcharts/index.d.ts" />
 /// <reference path="browser/ambient/jquery/index.d.ts" />
-/// <reference path="browser/ambient/mustache/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,4 +1,2 @@
-/// <reference path="main/ambient/chartjs/index.d.ts" />
 /// <reference path="main/ambient/highcharts/index.d.ts" />
 /// <reference path="main/ambient/jquery/index.d.ts" />
-/// <reference path="main/ambient/mustache/index.d.ts" />
